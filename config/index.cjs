463344fdafd676dccf5353059df1d56594be49f3
王安 <55909166+aries98@users.noreply.github.com>
2022-10-12 10:46:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -39,7 +39,7 @@ const USER_CONFIG = {
   USERS: [
     {
       name: '炸鸡',
-      id: 'oplc568_mIKrWB9N5r2T7Li_ukuQ',
+      id: 'oplc56y_6rAnmyjRBE2pPsU_v_RQ',
       useTemplateId: '4Zp2W8mSNRSxRjUIGxLjMlSdblbFkKpAV2I2i9g8BGc',
       openUrl: ' ',
       festivals: [
@@ -62,16 +62,53 @@ const USER_CONFIG = {
     },
     {
       name: '亲爱的炸鸡宝贝',
-      id: 'oplc568_mIKrWB9N5r2T7Li_ukuQ',
+      id: 'oplc56y_6rAnmyjRBE2pPsU_v_RQ',
       useTemplateId: 'SLO-LCeMstDSZUR0tmKHhsoVAO_OLAcWaY1Oxh1fFA4',
       horoscopeDate: '08-06',
       horoscopeDateType: '今日',
       openUrl: ' '
     },
     {
-      name: '11',
-      id: 'oplc568_mIKrWB9N5r2T7Li_ukuQ',
-      useTemplateId: 'xonUZbqpitewDk7XwArd9ctapUHNxJ6GQZIB4zlE270',
+      name: '炸鸡作业',
+      id: 'oplc56y_6rAnmyjRBE2pPsU_v_RQ',
+      useTemplateId: '9saVkqGFAl9VwP7PT59t-Guvb30PSB3rAF3ZjphL6yw',
+      openUrl: ' '
+    },
+    {
+      name: '自己测试',
+      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
+      useTemplateId: '4Zp2W8mSNRSxRjUIGxLjMlSdblbFkKpAV2I2i9g8BGc',
+      openUrl: ' ',
+      festivals: [
+        {
+          type: '*生日', name: '宝贝', year: '2002', date: '08-06',
+        },
+      ],
+      customizedDateList: [
+        { keyword: 'love_day', date: '2021-03-02' },
+      ],
+      courseSchedule: [
+        ['09:00-12:00 大学英语C3'],
+        ['12:15-15:15 旅游服务管理'],
+        ['今天没课哦'],
+        ['12:15-15:15 专业英语'],
+        ['15:30-18:30 旅游市场，19:00-22:00 西方文化概论'],
+        ['今天没课哦'],
+        ['今天没课哦'],
+      ],
+    },
+    {
+      name: '自己测试',
+      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
+      useTemplateId: 'SLO-LCeMstDSZUR0tmKHhsoVAO_OLAcWaY1Oxh1fFA4',
+      horoscopeDate: '08-06',
+      horoscopeDateType: '今日',
+      openUrl: ' '
+    },
+    {
+      name: '自己测试',
+      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
+      useTemplateId: '9saVkqGFAl9VwP7PT59t-Guvb30PSB3rAF3ZjphL6yw',
       openUrl: ' '
     }
   ],
@@ -82,43 +119,6 @@ const USER_CONFIG = {
       name: '自己',
       id: 'oplc568_mIKrWB9N5r2T7Li_ukuQ',
     },
-    {
-      name: '炸鸡',
-      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
-      useTemplateId: '4Zp2W8mSNRSxRjUIGxLjMlSdblbFkKpAV2I2i9g8BGc',
-      openUrl: ' ',
-      festivals: [
-        {
-          type: '*生日', name: '宝贝', year: '2002', date: '08-06',
-        },
-      ],
-      customizedDateList: [
-        { keyword: 'love_day', date: '2021-03-02' },
-      ],
-      courseSchedule: [
-        ['09:00-12:00 大学英语C3'],
-        ['12:15-15:15 旅游服务管理'],
-        ['今天没课哦'],
-        ['12:15-15:15 专业英语'],
-        ['15:30-18:30 旅游市场，19:00-22:00 西方文化概论'],
-        ['今天没课哦'],
-        ['今天没课哦'],
-      ],
-    },
-    {
-      name: '亲爱的炸鸡宝贝',
-      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
-      useTemplateId: 'SLO-LCeMstDSZUR0tmKHhsoVAO_OLAcWaY1Oxh1fFA4',
-      horoscopeDate: '08-06',
-      horoscopeDateType: '今日',
-      openUrl: ' '
-    },
-    {
-      name: '11',
-      id: 'oplc560lfACW6yepbYv7R5zGIEfg',
-      useTemplateId: 'xonUZbqpitewDk7XwArd9ctapUHNxJ6GQZIB4zlE270',
-      openUrl: ' '
-    }
   ],
   
   SLOT_LIST: [
